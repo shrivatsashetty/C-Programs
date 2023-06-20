c416c9770587a8af4aa0ddf291eec033a7ad54fe
shrivatsashetty <shrivatsa307@gmail.com>
2023-06-20 23:04:52 +0530

    ASCII using C

---
+++ Chapter 1/ascii_values.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+
+    printf("ASCII value of A is %d\n",'A');
+    printf("ASCII value of Z is %d\n",'Z');
+    printf("ASCII value of a is %d\n",'a');
+    printf("ASCII value of z is %d\n\n",'z');
+
+    printf("char corresponding to ASCII 91 is %c\n",91);
+    printf("char corresponding to ASCII 92 is %c\n",92);
+    printf("char corresponding to ASCII 93 is %c\n",93);
+    printf("char corresponding to ASCII 94 is %c\n",94);
+    printf("char corresponding to ASCII 95 is %c\n",95);
+    printf("char corresponding to ASCII 96 is %c\n",96);
+
+    printf("no of enlish alphabets in english %d\n\n",'z'-'a' + 1);
+
+    printf("ASCII value of ! is %d\n",'!');
+    printf("ASCII value of @ is %d\n",'@');
+    
+}
