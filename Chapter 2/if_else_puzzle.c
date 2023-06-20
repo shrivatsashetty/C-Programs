e2116aa6e44578e46ef877e27d6193b188862a2d
shrivatsashetty <shrivatsa307@gmail.com>
2023-06-20 23:17:04 +0530

    coding riddle

---
+++ Chapter 2/if_else_puzzle.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main() {
+    int x = 10;
+    if(x=1){
+    printf("x is one\n");
+    printf("%d\n",x);
+    }
+    else{
+        printf("x is not 1\n");
+        
+    }
+
+    return 0;
+}
