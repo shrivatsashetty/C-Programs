24e3e93f31aebe3c865c6f357c43edd0048f5bf7
shrivatsashetty <shrivatsa307@gmail.com>
2023-06-20 21:52:10 +0530

    fibbonacci using recursion

--- Chapter3/fibbonacci_recursion.c
+++ Chapter3/fibbonacci_recursion.c
@@ -5,10 +5,11 @@ void fibbonacci(int n);
 int a=0,b=1,buffer;
 int main(){
     int n;
-    printf("Enter how many terms: ");
+    printf("=====FIBBONACCI SERIES=====\n\nEnter how many terms: ");
     scanf("%d",&n);
-    printf("%d",0);
+    printf("%d,",0);
     fibbonacci(n);
+    printf("\b \n"); // '\b' is an escape sequence to backspace last character
     return 0;
 }
 
@@ -17,7 +18,7 @@ void fibbonacci(int n){
     
     if(n == 1){return;}  
     buffer= 0;
-    printf(" %d",b);
+    printf(" %d,",b);
     buffer = a;
     a=b;
     b+=buffer;
