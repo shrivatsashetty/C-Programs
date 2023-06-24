f30f5fad58ac29ac876de40ebd6f12cdb26f8711
shrivatsashetty <shrivatsa307@gmail.com>
2023-06-24 20:22:48 +0530

    intro to pointers

--- Chapter-4/pointer_intro.c
+++ Chapter-4/pointer_intro.c
@@ -2,9 +2,16 @@
 
 int main(){
 
-    int myage = 22;
-    int *myptr = &myage;
-    printf("pointer value: %p\n",&myptr); // 0x7ffc1704d564 or some hex value
+    int myage = 23; // an int type variable
+    int* myptr; // declaring a pointer that can store memory address of an int type variable
+    myptr = &myage;
+    printf("&myage : %p\n",&myage);
+    printf("&myptr : %p\n",&myptr); // 0x7ffc1704d564 or some hex value
     int value_at = *myptr;
+    printf("value_at = *myptr\n");
+    printf("value_at : %d\n",value_at);
+    printf("*myptr : %d\n",*myptr);
+    printf("*(&myage) : %d\n",*(&myage));
+    // printf("");
 
 }
---
+++ Chapter-4/pointer_intro_2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    float mynumber; // declared but not assigned with a value 
+    float *mypointer ; // we can also declare pointer as 'float* mypointer'
+    mypointer = &mynumber; 
+    printf("'mynumber' is the variable\n'mypointer' is the pointer pointing to the address of mynumber\n");
+
+    printf("adress of myvariable stored inside mypointer is %p\n",mypointer);
+    printf("value stored inside myvariable accessed via mypointer is %f\n",*mypointer);
+    printf("memory address of pointer 'mypointer' itself is %p\n",&mypointer);    
+
+    return 0;
+}
