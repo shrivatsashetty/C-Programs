816981bafa78d853479e4ed16c8167367dd6417b
shrivatsashetty <shrivatsa307@gmail.com>
2023-06-24 20:25:00 +0530

    pointer intro

--- Chapter-4/pointer_intro_2.c
+++ Chapter-4/pointer_intro_2.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 
 int main(){
-    float mynumber; // declared but not assigned with a value 
+    float mynumber = 21.022001;
     float *mypointer ; // we can also declare pointer as 'float* mypointer'
     mypointer = &mynumber; 
     printf("'mynumber' is the variable\n'mypointer' is the pointer pointing to the address of mynumber\n");
 
     printf("adress of myvariable stored inside mypointer is %p\n",mypointer);
-    printf("value stored inside myvariable accessed via mypointer is %f\n",*mypointer);
+    printf("value stored inside myvariable accessed via '*mypointer' is %f\n",*mypointer);
     printf("memory address of pointer 'mypointer' itself is %p\n",&mypointer);    
 
     return 0;
