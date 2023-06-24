c0bd8f702bfc329cd8895052ea148663abb873c2
shrivatsashetty <shrivatsa307@gmail.com>
2023-06-24 10:49:21 +0530

    script to rename

--- rename.sh
+++ rename.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 for dir in C*
-do 
-echo "$dir"
-new_name="$(echo "$dir" | sed 's/ /-/g')"
-mv "$dir" "$new_name"
-echo "$new_name"
-done
+    do 
+        echo "$dir"
+        new_name="$(echo "$dir" | sed 's/ /-/g')"
+        mv "$dir" "$new_name"
+        echo "$new_name"
+    done
