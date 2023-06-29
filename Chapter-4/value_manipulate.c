be90bb211debe0e4d7a7f52435ee045f2eb8fbff
shrivatsashetty <shrivatsa307@gmail.com>
2023-06-29 21:29:51 +0530

    vlue manipulation

--- Chapter-4/value_manipulate.c
+++ Chapter-4/value_manipulate.c
@@ -1,13 +1,26 @@
+/* In this program we will see how to manipulate 
+    value of a variable using pointers
+
+*/  
+
 #include <stdio.h>
 int main(){
-    int* ptr;
     int x;
+    int* xptr;
 
-    ptr = &x;
-    *ptr = 2;
+    xptr = &x;
+    *xptr = 2;
     printf("x = %d\n",x); // x = 2
-    *ptr++;
+    (*xptr)++; // x = 3
     printf("x = %d\n",x);
     *(&x)+=3;
     printf("x = %d\n",x);
+    // garbage value is stored inside uninitialised pointers
+    int y;
+    y = 21;
+    int* yptr;
+    printf("yptr = %p\n",yptr);
+    
+    
+    return 0;
 }
