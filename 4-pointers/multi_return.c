a3616b87e882f2a7c57f5ca2308a47d6e6e43a32
shrivatsashetty <shrivatsa307@gmail.com>
2023-07-02 20:40:07 +0530

    multi return using pointer

--- 4-pointers/multi_return.c
+++ 4-pointers/multi_return.c
@@ -3,16 +3,17 @@
 void multi_function(int a, int b, float* avg, int* sum, int* product);
 
 int main(){
+    
     int a = 15, b = 10, sum, product;
     float avg;
-    int* sumptr = &sum;
-    int* productptr = &product;
-    float* avgptr = &avg;
+    
     printf("value of a is %d and b is %d\n",a,b);
-    multi_function(a, b, avgptr, sumptr, productptr);
+    
+    multi_function(a, b, &avg, &sum, &product);
+    
     printf("Their sum is %d\n", sum);
-    printf("Their average is %f\n", *avgptr);
-    printf("Their product is %d\n", product);
+    printf("Their average is %f\n",avg);
+    printf("Their product is %d\n",product);
 
 
 
