19ec6cc03dfda44b187614d6ee813a9422ab8178
Shrivatsa Shetty <126042402+shrivatsashetty@users.noreply.github.com>
2023-08-26 16:07:01 +0530

    simple  helloworld C porg

--- 1-basics/hello.c
+++ 1-basics/hello.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
 int main(){
-  printf("Hello World\nfrom C program in git repo\nnow ina docker container");
+  printf("Hello World\nfrom C program in git repo\nnow ina docker container\n");
   return 0;
 }
